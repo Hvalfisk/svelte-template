6fe855809a4d76c493e1b8e32bf2d052f45b743d
Jakob Kofod Beyer <jakob@hvalfisk.dk>
2021-07-07 00:55:53 +0200

    all things

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
-<script>
-	export let name;
+<script lang="ts">
+  import { interval, Observable, scan, startWith, takeWhile, withLatestFrom } from 'rxjs';
+  import { map } from 'rxjs/operators';
+
+  const direction$: Observable<1 | -1> = interval(1000).pipe(map(() => Math.random() > 0.3 ? 1 : -1), startWith(1));
+  const counter$: Observable<number> =
+    interval(1000)
+      .pipe(
+        startWith(0),
+        withLatestFrom(direction$, (_, d) => d),
+        scan((acc, d) => acc + d, 0),
+        takeWhile(i => i <= 10)
+      );
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h2>Count to 10</h2>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{$counter$}
